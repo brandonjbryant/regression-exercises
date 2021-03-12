bcb02e452819d9761c56fdf4499cbdf3d0ce48a8
“Brandon <brandon.j.bryant1@gmail.com>
2021-03-12 13:46:47 -0600

    fixed

--- wrangle.py
+++ wrangle.py
@@ -1,69 +1,90 @@
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def acquire_telco():
+import os
+
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
     '''
-    Grab our data from path and read as csv
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
     '''
-    
-    df = pd.read_csv('telco_churn.csv')
-    return(df)
-    
-def clean_telco(df):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def new_telco_data():
     '''
-    Takes in a df of telco_data and cleans the data appropriatly by dropping nulls,
-    removing white space,
-    creates dummy variables for Contract type,
-    converts data to numerical, and bool data types, 
-    and drops columsn that are not needed.
-    
-    
-    return: df, a cleaned pandas data frame.
+    This function reads the telco_churn data from the Codeup db into a df,
+    write it to a csv file, and returns the df.
     '''
+    # Create SQL query.
+    sql_query = ''' select customer_id, monthly_charges, tenure, total_charges
+                    from customers
+                    where contract_type_id = 3;
+                    '''
     
-    # Instead of using dummies to seperate contracts use, 
-    # df[['Contract']].value_counts()
-    # Use a SQL querry
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
     
-    df = df
-    df.TotalCharges = df.TotalCharges.replace(r'^\s*$', np.nan, regex = True)
-    df = df.fillna(0)
-    dummy_df = pd.get_dummies(df[["Contract"]], drop_first=True)
-    df = pd.concat([df, dummy_df], axis=1)
-    df['Contract_Two year'] = df['Contract_Two year'].astype(bool)
-    df = df.loc[df['Contract_Two year'], :]
-    df = df.drop(columns = ['gender','SeniorCitizen',
-                            'Partner','Dependents',
-                            'PhoneService','MultipleLines',
-                            'InternetService','OnlineSecurity',
-                            'OnlineBackup','DeviceProtection',
-                            'TechSupport','StreamingTV',
-                            'StreamingMovies','PaperlessBilling',
-                            'PaymentMethod','Contract_One year',
-                            'Contract','Churn', 'Contract_Two year'])
-    df = df.set_index("customerID")         
     return df
-    
-def split_telco(df):
+
+def acquire_telco(cached=True):
     '''
-    Takes in a cleaned df of telco data and splits the data appropriatly into train, validate, and test.
+    This function reads in telco_churn data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in telco df from
+    a csv file, returns df.
     '''
+    if cached == False or os.path.isfile('telco_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_telco_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('telco_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('telco_df.csv', index_col=1)
+        df = df.iloc[:,1:]
+        # df = df.drop(columns='Unnamed: 0')
+        
+    return df
+
+def clean_telco(df):
+    '''Takes in a df of telco data and cleans the data by replacing blanks and dropping null values. 
+    The total_charges column is then converted to float
     
-    train_val, test = train_test_split(df, train_size =  0.8, random_state = 123)
-    train, validate = train_test_split(df, train_size =  0.8, random_state = 123)
+    return: df, a cleaned pandas dataframe'''
+    df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+    df = df.dropna()
+    df['total_charges'] = df.total_charges.astype(float)
+    return df
+
+def split_telco(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate[stratify_by])
+    
     return train, validate, test
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def wrangle_telco():
+def wrangle_telco(split=False):
     '''
-    wrangle_grades will read in our telco data as a pandas dataframe,
-    clean the data,
-    split the data,
-    return: train, validate, test sets of pandas dataframes for tleco data, strat on total charges
+    wrangle_telco will read in our student grades as a pandas dataframe,
+    clean the data
+    split the data
+    return: train, validate, test sets of pandas dataframes from telco if split = True
     '''
     df = clean_telco(acquire_telco())
-    return split_telco(df)
-
-# Use train.index = train.customerID to make index the id and then you can drop the customerId column when running regressions.
+    if split == True:
+        return split_telco(df)
+    else:
+        return df
