552195063f9917f36141211571f6ab9a571bfae2
“Brandon <brandon.j.bryant1@gmail.com>
2021-03-15 14:35:06 -0500

    regression prep

--- prepare.py
+++ prepare.py
@@ -1,25 +1,19 @@
-def Standard_Scaler(X_train, X_validate, X_test):
-    """
-    Takes in X_train, X_validate and X_test dfs with numeric values only
-    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs
-    """
+import pandas as pd
+import sklearn.preprocessing
 
-    scaler = sklearn.preprocessing.StandardScaler().fit(X_train)
-    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
-    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
-    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+def scale_data(train,validate,test):
+    '''Accepts train, validate, test data frames and applies min-max scaler
+    return: train, validate, test scaled pandas dataframe'''
     
-    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
-
-
-    def Min_Max_Scaler(X_train, X_validate, X_test):
-    """
-    Takes in X_train, X_validate and X_test dfs with numeric values only
-    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs 
-    """
-    scaler = sklearn.preprocessing.MinMaxScaler().fit(X_train)
-    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
-    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
-    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    scaler.fit(train)
     
-    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+    train_scaled = scaler.transform(train)
+    validate_scaled = scaler.transform(validate)
+    test_scaled = scaler.transform(test)
+    
+    train_scaled = pd.DataFrame(train_scaled, columns=train.columns)
+    validate_scaled = pd.DataFrame(validate_scaled, columns=train.columns)
+    test_scaled = pd.DataFrame(test_scaled, columns=train.columns)
+    
+    return train_scaled, validate_scaled, test_scaled
